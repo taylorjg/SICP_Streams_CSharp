e538e9c97f696c56589273438eedd095e0eb1463
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-05-27 20:06:49 +0100

    Added ToEnumerable() extension method and a first unit test

---
+++ Code/StreamExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Code
+{
+    public static class StreamExtensions
+    {
+        public static IEnumerable<int> ToEnumerable(this Stream s)
+        {
+            var currentStream = s;
+            for (;;)
+            {
+                if (currentStream.IsEmpty) yield break;
+                yield return currentStream.StreamCar;
+                currentStream = currentStream.StreamCdr;
+            }
+        }
+    }
+}
---
+++ Tests/StreamsTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Code;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    internal class StreamsTests
+    {
+        [Test]
+        public void TestIntegers()
+        {
+            var actual = Streams.Integers().ToEnumerable().Take(5).ToList();
+            Assert.That(actual, Is.EqualTo(new[] {1, 2, 3, 4, 5}));
+        }
+    }
+}
