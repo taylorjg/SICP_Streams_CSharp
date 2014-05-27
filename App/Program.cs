f2deb7bab5090f9d143eb0b4b58a5fbb73aebf8d
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-05-27 15:12:20 +0100

    Initial commit

---
+++ App/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace App
+{
+    class Program
+    {
+        static void Main()
+        {
+            var s = Stream.ConsStream(1, () => Stream.ConsStream(2, () => Stream.ConsStream(3, () => Stream.ConsStream(4, () => Stream.EmptyStream))));
+            DisplayStream(s);
+            DisplayStreamN(s, 2);
+
+            DisplayStreamN(Ones(), 10);
+            DisplayStreamN(IntegersStartingFrom(15), 10);
+            DisplayStreamN(Integers(), 10);
+        }
+
+        private static Stream Ones()
+        {
+            return Stream.ConsStream(1, Ones);
+        }
+
+        private static Stream IntegersStartingFrom(int n)
+        {
+            return Stream.ConsStream(n, () => IntegersStartingFrom(n + 1));
+        }
+
+        private static Stream Integers()
+        {
+            return IntegersStartingFrom(1);
+        }
+
+        private static void DisplayStream(Stream s)
+        {
+            if (s.IsEmpty)
+            {
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.Write("{0} ", s.StreamCar);
+                DisplayStream(s.StreamCdr);
+            }
+        }
+
+        private static void DisplayStreamNIter(Stream s, int remaining)
+        {
+            if (remaining > 0)
+            {
+                if (s.IsEmpty)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write("{0} ", s.StreamCar);
+                    DisplayStreamNIter(s.StreamCdr, remaining - 1);
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+
+        private static void DisplayStreamN(Stream s, int n)
+        {
+            DisplayStreamNIter(s, n);
+        }
+    }
+}
---
+++ App/Stream.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace App
+{
+    public sealed class Stream
+    {
+        private Stream()
+        {
+        }
+
+        public static Stream EmptyStream = new Stream();
+
+        public static Stream ConsStream(int head, Func<Stream> delayedStream)
+        {
+            return new Stream
+                {
+                    _head = new ValueHolder(head),
+                    _delayedStream = delayedStream
+                };
+        }
+
+        public bool IsEmpty { get { return this == EmptyStream; } }
+
+        public int StreamCar
+        {
+            get
+            {
+                if (_head == null) throw new InvalidOperationException("StreamCar of empty stream.");
+                return _head.Value;
+            }
+        }
+
+        public Stream StreamCdr
+        {
+            get
+            {
+                if (_delayedStream == null) throw new InvalidOperationException("StreamCdr of empty stream.");
+                return _delayedStream();
+            }
+        }
+
+        private ValueHolder _head;
+        private Func<Stream> _delayedStream;
+
+        private class ValueHolder
+        {
+            public ValueHolder(int value)
+            {
+                Value = value;
+            }
+
+            public int Value { get; private set; }
+        }
+    }
+}
